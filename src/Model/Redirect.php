69105c9faeb809f75c75620033d89b8b58c31c3c
Werner Freytag <freytag@gmx.de>
2024-07-29 11:25:59 +0200

    make Redirect class readonly

--- src/Model/Redirect.php
+++ src/Model/Redirect.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Pecotamic\CreateURLs\Model;
 
-class Redirect
+readonly class Redirect
 {
-    public readonly string $requestUri;
-    public readonly string $target;
+    public string $requestUri;
+    public string $target;
 
     public function __construct(
         string $requestUri,
         string $target,
-        readonly string $responseCode = '301'
+        public string $responseCode = '301'
     ) {
         assert(in_array($responseCode, ['301', '302']));
 
